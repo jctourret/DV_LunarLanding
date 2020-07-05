4f61bd5e021c3437f0f97b0ae5ee42ed75e013b5
Juan Cruz Tourret <jctourret@gmail.com>
2020-07-04 21:26:47 -0300

    Comienzo del proyecto y controles básicos

---
+++ Assets/Scripts/Landing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Landing : MonoBehaviour
+{
+    float scoreWorth = 50;
+
+    Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            if (collision.collider){
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    RaycastHit downRay;
+
+    float hSpeed;
+    float vSpeed;
+    float altitud;
+    float fuel = 1000.0f;
+
+    Rigidbody2D rb;
+    Quaternion rotation;
+
+    public float rocketProp = 1f;
+    public float gravity = 0.16f;
+    public float fuelConsumption = 1.0f;
+    public float rotationSpeed = 100;
+    public float currentRotation = 0;
+    bool rocketOn;
+    
+    private void Start(){
+        rocketOn = false;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update(){
+        altitud = transform.position.y;
+        hSpeed = rb.velocity.x;
+        vSpeed = rb.velocity.x;
+        rb.gravityScale = gravity;
+        Debug.DrawRay(transform.position, Vector3.down, Color.yellow);
+
+        if (Input.GetKey("space")){
+            rocketOn = true;
+        }
+        else{
+            rocketOn = false;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
+            currentRotation -= rotationSpeed * Time.deltaTime;
+            rotation = Quaternion.Euler(new Vector3(0, 0, currentRotation));
+            transform.rotation = rotation;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
+            currentRotation += rotationSpeed * Time.deltaTime;
+            rotation = Quaternion.Euler(new Vector3(0, 0, currentRotation));
+            transform.rotation = rotation;
+        }
+    }
+    private void FixedUpdate(){
+        if (rocketOn) {
+            rb.AddRelativeForce(new Vector2(0, rocketProp));
+            if (rb.velocity.x > 0) {
+                rb.AddRelativeForce(new Vector2(-rocketProp/2, 0));
+            }
+            if (rb.velocity.x < 0)
+            {
+                rb.AddRelativeForce(new Vector2(+rocketProp/2, 0));
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Landing")
+        {
+        }
+    }
+}
